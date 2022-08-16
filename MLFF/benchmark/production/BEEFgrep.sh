b23d5ccccab19e425a333d3088d911cbd722db01
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2022-08-16 12:54:38 +0100

    Create BEEFgrep.sh

---
+++ MLFF/benchmark/production/BEEFgrep.sh
@@ -0,0 +1,9 @@
+#!/bin/bash -l
+
+steps =  #nb of steps or NSW
+
+for T in #temperature directories
+do
+        grep BEEF ${T}/ML_LOGFILE | tail -n ${steps} | awk '{print $4}' > BEEF${T}.dat
+        #grep BEEF ${T}/sequential_run2/ML_LOGFILE | tail -n ${steps} | awk '{print $4}' >> BEEF${T}.dat
+done
