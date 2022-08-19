44185b54c4e0dc6a81a70887ba093e556890837e
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2022-08-19 11:45:41 +0100

    Update grep_fe.sh

--- MLFF/benchmark/unseen_data/grep_fe.sh
+++ MLFF/benchmark/unseen_data/grep_fe.sh
@@ -5,7 +5,7 @@ L = #number_of_atoms + 1
 for T in #new_temeprature_directories
 do
 	cd $T
-	for NUM in {0..199}
+	for NUM in {0..XXX} #XXX is the number of structures per temperature (n_seed*sample) minus 1
 	do 
 		cd $NUM
 		grep TOTAL-FORCE -A ${L} OUTCAR > positions-forces.dat
