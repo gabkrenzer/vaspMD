e59505219e3f2c43a4992b2d08763fe0a820023c
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2022-08-19 11:14:13 +0100

    Update BEEFgrep.sh

--- MLFF/benchmark/production/BEEFgrep.sh
+++ MLFF/benchmark/production/BEEFgrep.sh
@@ -5,5 +5,5 @@ steps =  #nb of steps or NSW
 for T in #temperature directories
 do
         grep BEEF ${T}/ML_LOGFILE | tail -n ${steps} | awk '{print $4}' > BEEF${T}.dat
-        #grep BEEF ${T}/sequential_run2/ML_LOGFILE | tail -n ${steps} | awk '{print $4}' >> BEEF${T}.dat
+        #grep BEEF ${T}/sequential_run2/ML_LOGFILE | tail -n ${steps} | awk '{print $4}' >> BEEF${T}.dat - use as many as there are sequential runs
 done
