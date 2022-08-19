448d74ccebb253d28ab9f739a727e9555a1f394f
Gabriel Krenzer <52633008+gabkrenzer@users.noreply.github.com>
2022-08-19 12:14:58 +0100

    Update sp.sh

--- MLFF/benchmark/unseen_data/sp.sh
+++ MLFF/benchmark/unseen_data/sp.sh
@@ -5,25 +5,25 @@ for T in #new_temperatures_directories
 do 
 	echo $T
 	mkdir $T 
-	cp INCAR_MLFF KPOINTS POTCAR ML_FF mlff_sp.job $T
-        cp INCAR_DFT dft_sp.job $T
+	cp INCAR_MLFF KPOINTS POTCAR ML_FF mlff_sp.job $T #update with MLFF single-point job name
+        cp INCAR_DFT dft_sp.job $T #update with DFT single-point job name
         cp structures/${T}POSCAR* $T
 	cd $T
-        for NUM in {0..199}
+        for NUM in {0..XXX} #XXX is the number of structures per temperature (n_seed*sample) minus 1
 	do
 		echo $NUM
 		mkdir $NUM
-		cp INCAR_MLFF KPOINTS POTCAR ML_FF mlff_sp.job $NUM
+		cp INCAR_MLFF KPOINTS POTCAR ML_FF mlff_sp.job $NUM #update with MLFF single-point job name
 		mv ${T}POSCAR$NUM $NUM/POSCAR
 		mv INCAR_MLFF INCAR
 		mkdir ${NUM}/DFT
-	        cp dft_sp.job ${NUM}/DFT
+	        cp dft_sp.job ${NUM}/DFT #update with DFT single-point job name
                 cp INCAR_DFT ${NUM}/DFT/INCAR
   		cd $NUM
-		qsub mlff_sp.job
+		qsub mlff_sp.job #update with MLFF single-point job name
 		cp POSCAR KPOINTS POTCAR DFT
 		cd DFT
-		qsub dft_sp.job
+		qsub dft_sp.job #update with DFT single-point job name
 		cd ../..
 	done
 	cd ..
